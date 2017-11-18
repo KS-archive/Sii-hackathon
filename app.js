2fe74b181a9c4d8f75b3c1f0cfc64d93260b14ad
Piotrek <piotr.pietruszka@o2.pl>
2017-11-18 13:18:53 +0100

    attach socketio

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express();
 
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.static(path.join(__dirname, '/build')));
-app.use((req, res) => res.sendFile(`${__dirname}/build/index.html`));
-app.listen(3000);
+class Application{
+  constructor(){
+    this.app = express();
+    this.init();
+  };
+
+  init(){
+    this.middleware();
+    this.routes();
+    this.sockets();
+  };
+  middleware(){
+    let app = this.app;
+
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.json());
+
+    app.use(express.static(path.join(__dirname, '/public')));
+    app.use(express.static(path.join(__dirname, '/build')));
+  };
+  routes(){
+    let app = this.app;
+
+    app.get('/', function(req, res) {
+      res.sendFile(`${__dirname}/build/index.html`);
+    });
+
+  };
+  sockets(){
+    const http = require('http').Server(this.app);
+    this.io = require('socket.io')(http);
+
+    http.listen(3000, function() {
+      console.log('listening on *:3000');
+    });
+  };
+};
+
+
+new Application();
